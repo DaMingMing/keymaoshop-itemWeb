621fc72d35af27ca18763d190a84a8a5c2fe4d42
DaMingMing <1044096131@qq.com>
2019-07-03 00:36:19 +0800

    产品单页新增登录拦截

--- src/main/webapp/js/e3mall.js
+++ src/main/webapp/js/e3mall.js
@@ -1,6 +1,6 @@
 var E3MALL = {
 	checkLogin : function(){
-		var _ticket = $.cookie("TT_TOKEN");
+		var _ticket = $.cookie("token");
 		if(!_ticket){
 			return ;
 		}
@@ -8,13 +8,14 @@ var E3MALL = {
 			url : "http://localhost:8088/user/token/" + _ticket,
 			dataType : "jsonp",
 			type : "GET",
-			success : function(data){
-				if(data.status == 200){
-					var username = data.data.username;
-					var html = username + "，欢迎来到宜立方购物网！<a href=\"http://www.e3mall.cn/user/logout.html\" class=\"link-logout\">[退出]</a>";
-					$("#loginbar").html(html);
-				}
-			}
+            success : function(data){
+                if(data.status == 200){
+                    var username = data.data.username;
+                    var html = username + "，欢迎来到keymao购物网！<a href=\"http://localhost:8088/user/logout.html?token="+ _ticket  + "\" class=\"link-logout\">[退出]</a>";
+                    //var html = `${username}，欢迎来到keymao购1物网！<a href="localhost:8088/user/logout.html?token=${_ticket}" class="link-logout">[退出]</a>`;
+                    $("#loginbar").html(html);
+                }
+            }
 		});
 	}
 }
